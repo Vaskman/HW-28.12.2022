c102988e1af56828c21a1cfe7f63976e307c764b
Vaskman <z.vaskman@gmail.com>
2022-12-28 13:17:38 +0300

    Задача №1

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,21 @@ public class Main {
 
     public static void task1() {
         System.out.println("ЗАДАЧА №1");
+        byte pen = 3;
+        System.out.println("Ручка "+ pen+" шт");
+        short pencil = 32000;
+        System.out.println("Карандаш "+pencil+" шт");
+        int marker = 3000000;
+        System.out.println("Маркер "+ marker+" шт");
+        long paperclips = 300000000;
+        System.out.println("Скрепки "+paperclips+" шт");
+        float ink = 0.88F;
+        System.out.println("Чернил "+ ink+" мл");
+        double paper = 10.555;
+        System.out.println("Бумага "+ paper+" шт");
+
+
+
 
     }
 
