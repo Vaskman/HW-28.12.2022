a91a00d7c65741611ab50e1c50fb2fd350cdb61c
Vaskman <z.vaskman@gmail.com>
2022-12-28 13:48:53 +0300

    Задача №2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Home Work 28.12.2022");
         task1();
+       task2();
+
     }
 
     public static void task1() {
@@ -12,17 +14,29 @@ public class Main {
         System.out.println("Карандаш "+pencil+" шт");
         int marker = 3000000;
         System.out.println("Маркер "+ marker+" шт");
-        long paperclips = 300000000;
+        long paperclips = 3000000;
         System.out.println("Скрепки "+paperclips+" шт");
         float ink = 0.88F;
         System.out.println("Чернил "+ ink+" мл");
         double paper = 10.555;
         System.out.println("Бумага "+ paper+" шт");
-
-
-
-
     }
-
+    public static void task2(){
+        System.out.println("ЗАДАЧА №2");
+        float a=27.12F;
+        System.out.println("а = "+a);
+        long b=987678965549L;
+        System.out.println("b = "+b);
+        float c=2.786F;
+        System.out.println("c = "+c);
+        short d=569;
+        System.out.println("d = "+d);
+        short e=-159;
+        System.out.println("e = "+e);
+        int f=27897;
+        System.out.println("f = "+f);
+        byte g=67;
+        System.out.println("g = "+g);
+    }
 
 }
