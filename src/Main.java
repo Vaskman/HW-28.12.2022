feec791cdf3119a8b46fac179e3b1c3405b7466f
Vaskman <z.vaskman@gmail.com>
2022-12-28 13:00:22 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-    System.out.println("Home Work 28.12.2022");
+        System.out.println("Home Work 28.12.2022");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("ЗАДАЧА №1");
 
     }
+
+
 }
